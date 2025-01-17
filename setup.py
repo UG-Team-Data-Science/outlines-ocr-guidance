de5b1d56dec87b763058ed43d7f829654245237b
H.T. Kruitbosch <H.T.Kruitbosch@rug.nl>
2025-01-17 13:59:16 +0100

    v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='outlines_ocr_guidance',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=["outlines>=0.0.46", ],
     author='H.T. Kruitbosch',
